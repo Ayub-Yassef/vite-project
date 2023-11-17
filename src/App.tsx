62e62c029ec399632fc2511e41e72d848e25923d
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2023-11-16 20:23:04 -0800

    attempting app for notetaking

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, ChangeEventHandler, useEffect } from 'react';
+import NoteItem from './components/NoteItem';
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+type noteType = {
+  id: string;
+  title: string;
+  description?: string;
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+//let title = "";
+const App = () => {
+  // const [title, setTitle] = useState("");
+  // const [description, setDescription] = useState("");
+  
+  const [count, setCount] = useState(0);
+  const [noteToView, setNoteToView] = useState<noteType>();
+  const [notes, setNotes] = useState<noteType[]>([]);
+  const [values, setValues] = useState({
+    title: "",
+    description: "",
+  });
+  const [selectedNoteId, setSelectedNoteId] = useState("");
 
-export default App
+  const handleChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = ({ target }) => {
+    const { name, value } = target;
+    setValues({ ...values, [name]: value });
+  };
+
+};
