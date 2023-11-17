af0be1e175e6c4e63a11b81518521ba5d306aa7d
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2023-11-17 11:53:35 -0800

    basic but fully functioning notes app created

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootEl = document.getElementById("root") as HTMLElement;
+
+const root = ReactDOM.createRoot(rootEl);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
